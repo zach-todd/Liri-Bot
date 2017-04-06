050a5d3492ca94a16f2ab9a511dd4fad396e4ade
Zach Todd <gingergoalie@gmail.com>
2017-04-06 17:22:52 -0400

    initial setup

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<G7PR677veDI9FSopxtksU4oaG>',
+  consumer_secret: '<uFSuzmq2KZKRgdWiDUZeRLH8cWctGhezliLsQ1XpKIAHTO506E>',
+  access_token_key: '<2175206131-pCcszKMdn1Xt8W7tTGJFZ6q0rbo8vyjmpWWUyfR>',
+  access_token_secret: '<8NmkLjJqgvUlAt7up0IQtlzVhUS27dUxkc9zorr2hgp5C>',
+}
